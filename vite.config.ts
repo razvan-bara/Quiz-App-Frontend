a0c04c64239b4e705567dca545c9d86a3586d7be
razvan-bara <rzvbara@gmail.com>
2023-11-06 13:16:29 +0200

    configure project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+      '@services/': `${path.resolve(__dirname, 'src/services')}/`,
+      '@stores/': `${path.resolve(__dirname, 'src/stores')}/`,
+      '@ctypes/': `${path.resolve(__dirname, 'src/types')}/`,
+      '@views/': `${path.resolve(__dirname, 'src/views')}/`
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+      }
+    }
+  }
+})
