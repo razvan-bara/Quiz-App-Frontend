1b71e4f98b078df64fddf8f10e272af43cb84faa
razvan-bara <rzvbara@gmail.com>
2023-11-07 15:42:54 +0200

    more customizable error card

--- src/components/ErrorCard.vue
+++ src/components/ErrorCard.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-const props = defineProps(['title', 'message'])
+
+import {computed} from "vue";
+
+const props = defineProps({
+  title: {type: String, required: true},
+  message: {type: String, required: true},
+  eclass : {type: Number, required: false}
+})
+
+const classObject = computed(() => ({
+  'is-info' :   props.eclass === 0,
+  'is-danger' : props.eclass === undefined,
+  'is-warning': props.eclass === 1
+}))
 </script>
 
 <template>
-  <section class="hero is-large is-danger">
+  <section class="hero is-large" :class="classObject">
   <div class="hero-body">
     <p class="title">
       {{ props.title }}
